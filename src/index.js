04e53553821089149584b49bc2b77f546ab57cbf
Gonjanat <gonjie@Milos-Laptop.local>
2024-11-02 21:21:59 +0700

    change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppWrapper from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <AppWrapper />
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
